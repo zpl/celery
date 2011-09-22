251c1bdc768fc613712d592fb0ed1b168341990d
Ask Solem <ask@celeryproject.org>
2011-09-22 15:59:52 +0100

    Statedb was not saved at exit

--- celery/worker/__init__.py
+++ celery/worker/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import atexit
 import logging
 import socket
 import sys
@@ -153,9 +154,8 @@ class WorkController(object):
         self._finalize_db = None
 
         if self.db:
-            persistence = state.Persistent(self.db)
-            self._finalize_db = Finalize(persistence, persistence.save,
-                                         exitpriority=5)
+            self._persistence = state.Persistent(self.db)
+            atexit.register(self._persistence.save)
 
         # Queues
         if self.disable_rate_limits:
--- celery/worker/state.py
+++ celery/worker/state.py
@@ -88,7 +88,8 @@ class Persistent(object):
         self._load()
 
     def save(self):
-        self.sync(self.db).sync()
+        self.sync(self.db)
+        self.db.sync()
         self.close()
 
     def merge(self, d):
@@ -102,7 +103,7 @@ class Persistent(object):
         return d
 
     def open(self):
-        return self.storage.open(self.filename)
+        return self.storage.open(self.filename, writeback=True)
 
     def close(self):
         if self._is_open:
@@ -111,7 +112,6 @@ class Persistent(object):
 
     def _load(self):
         self.merge(self.db)
-        self.close()
 
     @cached_property
     def db(self):
